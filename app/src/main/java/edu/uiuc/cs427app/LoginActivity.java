8512fb66cbc0f9c81972db65d2346de9f8d0bfc0
Preeti A <preetia3@illinois.edu>
2022-10-28 13:44:30 -0700

    Added Dark and Light Modes

--- app/src/main/java/edu/uiuc/cs427app/LoginActivity.java
+++ app/src/main/java/edu/uiuc/cs427app/LoginActivity.java
@@ -39,19 +39,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
+
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatDelegate;
+
+
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+   // private ActivityMainBinding binding;
     private EditText username;
     private EditText password;
     private Hashtable<String, String> accountDictionary;
     private SharedPreferences preferences;
     private EditText etUserName;
     private EditText etPassWord;
+
+    private RadioGroup radioGroup;
+    private TextView themeTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,11 +71,40 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         etUserName = findViewById(R.id.username);
         etPassWord = findViewById(R.id.password);
 
+        // initializing all our variables.
+        radioGroup = findViewById(R.id.idRGroup);
+        themeTV = findViewById(R.id.idtvTheme);
+
         Button buttonLogin = findViewById(R.id.buttonLogin);
         Button buttonSignUp = findViewById(R.id.buttonSignUp);
 
         buttonLogin.setOnClickListener(this);
         buttonSignUp.setOnClickListener(this);
+
+
+        // on below line we are setting on check change method for our radio group.
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // on radio button check change
+                switch (checkedId) {
+                    case R.id.idRBLight:
+                        // on below line we are checking the radio button with id.
+                        // on below line we are setting the text to text view as light mode.
+                        themeTV.setText("Light Theme");
+                        // on below line we are changing the theme to light mode.
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                        break;
+                    case R.id.idRBDark:
+                        // this method is called when dark radio button is selected
+                        // on below line we are setting dark theme text to our text view.
+                        themeTV.setText("Dark Theme");
+                        // on below line we are changing the theme to dark mode.
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                        break;
+                }
+            }
+        });
 
 
     }
