53d5474b7b7ac831c9e2c83f4759ec7c9ab13a1c
ChinuSaraf <csaraf2@illinois.edu>
2022-11-24 13:57:34 -0600

    Logout instrumented test added

--- app/src/main/java/edu/uiuc/cs427app/MainActivity.java
+++ app/src/main/java/edu/uiuc/cs427app/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         String username = getIntent().getStringExtra("username");
         String theme = getIntent().getStringExtra("theme");
-        if(theme.equals("1"))
+        if(nonNull(theme) && theme.equals("1"))
         {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         }
