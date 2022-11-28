e4151aebe2cd8e4e8d9a9d6d637f72eb2b6e1e35
Preeti A <preetia3@illinois.edu>
2022-11-28 15:46:01 -0800

    Complete Weather, partial Signuplogin

--- app/src/androidTest/java/edu/uiuc/cs427app/UserSignUpLoginTest.java
+++ app/src/androidTest/java/edu/uiuc/cs427app/UserSignUpLoginTest.java
@@ -2,6 +2,7 @@ package edu.uiuc.cs427app;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
@@ -23,6 +24,7 @@ import androidx.test.filters.LargeTest;
 import junit.framework.TestCase;
 
 import org.junit.FixMethodOrder;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
@@ -30,33 +32,50 @@ import org.junit.runners.MethodSorters;
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 @FixMethodOrder(MethodSorters.NAME_ASCENDING) //to run the tests in ascending order
+
 public class UserSignUpLoginTest extends TestCase{
     @Test
-    // test to check the sign up function
+    // test to check the login function
 
-    public void userSignUpTest() {
-        Intent intent = new Intent(ApplicationProvider.getApplicationContext(), MainActivity.class);
+    public void userLoginTest() {
+        Intent intent = new Intent(ApplicationProvider.getApplicationContext(), LoginActivity.class);
         intent.putExtra("username", "admin");
         intent.putExtra("password", "123456");
         intent.putExtra("theme", "1");
 
-        try (final ActivityScenario<MainActivity> scenario = ActivityScenario.launch(intent)) {
+        try (final ActivityScenario<LoginActivity> scenario = ActivityScenario.launch(intent)) {
             // this test is only for username and password matched with stored info in app
-            Thread.sleep(3000); //wait time of 3 sec
+            Thread.sleep(2000); //wait time of 3 sec
+            onView(withId(R.id.username)).perform(typeText("abc"));
+//                    .perform(scrollTo())
+//                    .perform(click());
+            Thread.sleep(2000); //wait time of 3 sec
+            Espresso.closeSoftKeyboard();
+            Thread.sleep(2000); //wait time of 3 sec
+            onView(withId(R.id.password)).perform(typeText("123"), closeSoftKeyboard());
+//            Espresso.closeSoftKeyboard();
+//            onView(withId(R.id.textView1)).perform(typeText("admin"), ViewActions.closeSoftKeyboard());
+//            onView(withId(R.id.textView2)).perform(typeText("123456"), ViewActions.closeSoftKeyboard());
+            Thread.sleep(2000); //wait time of 3 sec
+            onView(withId(R.id.buttonSignUp)).perform(click());
+            Thread.sleep(2000); //wait time of 3 sec
             onView(withId(R.id.buttonSignUp)).perform(ViewActions.click());
             // the text works well or not -> not sure.
+//            onView(withId(R.id.logoutButton)).perform(ViewActions.click());
+            Thread.sleep(5000); //wait time of 3 sec
             onView(withId(R.id.logoutButton)).check(ViewAssertions.matches(ViewMatchers.withText("@string/logoutButton")));
-            Thread.sleep(3000); //wait time of 3 sec
-        } catch (InterruptedException e) {
+            Thread.sleep(2000); //wait time of 3 sec
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
 
-    @Test
-    // test to check the login function
 
-    public void userLoginTest() {
+    @Test
+    // test to check the signup function
+
+    public void userSignUpTest() {
 
 
     }
--- app/src/androidTest/java/edu/uiuc/cs427app/WeatherTest.java
+++ app/src/androidTest/java/edu/uiuc/cs427app/WeatherTest.java
@@ -1,4 +1,57 @@
 package edu.uiuc.cs427app;
 
-public class WeatherTest {
+import static androidx.test.espresso.Espresso.onView;
+
+import android.content.Intent;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import junit.framework.TestCase;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+@FixMethodOrder(MethodSorters.NAME_ASCENDING) //to run the tests in ascending order
+public class WeatherTest extends TestCase {
+    @Test
+    //Test for city 1:Vancouver
+    public void testWeatherForVancouver() throws InterruptedException {
+        //using espresso Intent to validate and stubbing of intents sent out by the MapActivity under ApplicationProvided
+        Intent intent = new Intent(ApplicationProvider.getApplicationContext(), WeatherActivity.class);
+        intent.putExtra("cityName", "Vancouver");
+        intent.putExtra("username", "user1");
+        try (final ActivityScenario<WeatherActivity> scenario = ActivityScenario.launch(intent)) {
+            Thread.sleep(3000); //wait time of 3 sec
+            onView(ViewMatchers.withId(R.id.tv_get_cityName)).check(ViewAssertions.matches(ViewMatchers.withText("Vancouver")));
+//            onView(ViewMatchers.withId(R.id.latitude)).check(ViewAssertions.matches(ViewMatchers.withText("Latitude: 40.7127753")));
+//            onView(ViewMatchers.withId(R.id.longitude)).check(ViewAssertions.matches(ViewMatchers.withText("Longitude: -74.0059728")));
+            Thread.sleep(3000); //wait time of 3 sec
+        }
+    }
+
+    @Test
+    //Test for city 2:Chicago
+    public void testWeatherForChicago() throws InterruptedException {
+        //using espresso Intent to validate and stubbing of intents sent out by the MapActivity under ApplicationProvided
+        Intent intent = new Intent(ApplicationProvider.getApplicationContext(), WeatherActivity.class);
+        intent.putExtra("cityName", "Chicago");
+        intent.putExtra("username", "user1");
+        try (final ActivityScenario<WeatherActivity> scenario = ActivityScenario.launch(intent)) {
+            Thread.sleep(3000); //wait time of 3 sec
+            onView(ViewMatchers.withId(R.id.tv_get_cityName)).check(ViewAssertions.matches(ViewMatchers.withText("Chicago")));
+//            onView(ViewMatchers.withId(R.id.latitude)).check(ViewAssertions.matches(ViewMatchers.withText("Latitude: 40.7127753")));
+//            onView(ViewMatchers.withId(R.id.longitude)).check(ViewAssertions.matches(ViewMatchers.withText("Longitude: -74.0059728")));
+            Thread.sleep(3000); //wait time of 3 sec
+        }
+    }
+
 }
