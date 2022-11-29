8937703714c777ae7d61ab16158445cd1eeea64c
Henesys <hyosup2@illinois.edu>
2022-11-28 18:28:05 -0600

    Completed Signup Logic, Task 1 Completed

--- app/src/androidTest/java/edu/uiuc/cs427app/UserSignUpLoginTest.java
+++ app/src/androidTest/java/edu/uiuc/cs427app/UserSignUpLoginTest.java
@@ -3,6 +3,7 @@ package edu.uiuc.cs427app;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
@@ -24,7 +25,6 @@ import androidx.test.filters.LargeTest;
 import junit.framework.TestCase;
 
 import org.junit.FixMethodOrder;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
@@ -32,68 +32,70 @@ import org.junit.runners.MethodSorters;
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 @FixMethodOrder(MethodSorters.NAME_ASCENDING) //to run the tests in ascending order
-
 public class UserSignUpLoginTest extends TestCase{
     @Test
-    // test to check the login function
-
-    public void userLoginTest() {
-        Intent intent = new Intent(ApplicationProvider.getApplicationContext(), LoginActivity.class);
-        intent.putExtra("username", "admin");
-        intent.putExtra("password", "123456");
-        intent.putExtra("theme", "1");
-
-        try (final ActivityScenario<LoginActivity> scenario = ActivityScenario.launch(intent)) {
-            // this test is only for username and password matched with stored info in app
-            Thread.sleep(2000); //wait time of 3 sec
-            onView(withId(R.id.username)).perform(typeText("abc"));
-//                    .perform(scrollTo())
-//                    .perform(click());
-            Thread.sleep(2000); //wait time of 3 sec
-            Espresso.closeSoftKeyboard();
-            Thread.sleep(2000); //wait time of 3 sec
-            onView(withId(R.id.password)).perform(typeText("123"), closeSoftKeyboard());
-//            Espresso.closeSoftKeyboard();
-//            onView(withId(R.id.textView1)).perform(typeText("admin"), ViewActions.closeSoftKeyboard());
-//            onView(withId(R.id.textView2)).perform(typeText("123456"), ViewActions.closeSoftKeyboard());
-            Thread.sleep(2000); //wait time of 3 sec
-            onView(withId(R.id.buttonSignUp)).perform(click());
-            Thread.sleep(2000); //wait time of 3 sec
-            onView(withId(R.id.buttonSignUp)).perform(ViewActions.click());
-            // the text works well or not -> not sure.
-//            onView(withId(R.id.logoutButton)).perform(ViewActions.click());
-            Thread.sleep(5000); //wait time of 3 sec
-            onView(withId(R.id.logoutButton)).check(ViewAssertions.matches(ViewMatchers.withText("@string/logoutButton")));
-            Thread.sleep(2000); //wait time of 3 sec
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
-
-
-    @Test
-    // test to check the signup function
+    // test to check the sign up function
 
     public void userSignUpTest() {
         Intent intent = new Intent(ApplicationProvider.getApplicationContext(), LoginActivity.class);
-        intent.putExtra("username", "admin");
-        intent.putExtra("password", "123456");
-        intent.putExtra("theme", "1");
 
         try (final ActivityScenario<LoginActivity> scenario = ActivityScenario.launch(intent)) {
             // this test is only for username and password matched with stored info in app
-//            Thread.sleep(1000); //wait time of 3 sec
             onView(withId(R.id.username)).perform(typeText("abc"));
             Espresso.closeSoftKeyboard();
-            onView(withId(R.id.password)).perform(typeText("123"), closeSoftKeyboard());
+            onView(withId(R.id.password)).perform(typeText("123"));
+            Espresso.closeSoftKeyboard();
             onView(withId(R.id.buttonSignUp)).perform(click());
-            Thread.sleep(2000); //wait time of 2 sec
-            onView(withId(R.id.logoutButton)).perform(ViewActions.click());
-            Thread.sleep(2000); //wait time of 2 sec
+            Thread.sleep(1000); //wait time of 1 sec
+            onView(withId(R.id.logoutButton)).perform(click());
+            Thread.sleep(1000); //wait time of 1 sec
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
+    @Test
+    // test to check the login function
+
+    public void userLoginTest1() {
+        Intent intent = new Intent(ApplicationProvider.getApplicationContext(), LoginActivity.class);
+
+        try (final ActivityScenario<LoginActivity> scenario = ActivityScenario.launch(intent)) {
+            // this test is only for username and password matched with stored info in app
+            onView(withId(R.id.username)).perform(typeText("abc"));
+            Espresso.closeSoftKeyboard();
+            onView(withId(R.id.password)).perform(typeText("123"));
+            Espresso.closeSoftKeyboard();
+            onView(withId(R.id.buttonLogin)).perform(click());
+            Thread.sleep(1000); //wait time of 1 sec
+            onView(withId(R.id.logoutButton)).perform(click());
+            Thread.sleep(1000); //wait time of 1 sec
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    // test to check the login function
+
+    public void userLoginTest2() {
+        Intent intent = new Intent(ApplicationProvider.getApplicationContext(), LoginActivity.class);
+
+        try (final ActivityScenario<LoginActivity> scenario = ActivityScenario.launch(intent)) {
+            // this test is only for username and password matched with stored info in app
+            onView(withId(R.id.username)).perform(typeText("abc"));
+            Espresso.closeSoftKeyboard();
+            onView(withId(R.id.password)).perform(typeText("12"));
+            Espresso.closeSoftKeyboard();
+            onView(withId(R.id.buttonLogin)).perform(click());
+            Thread.sleep(1000); //wait time of 1 sec
+
+            // TODO: how to check we are still in the login page?
+            // one possible way is to check the login button below
+            onView(withId(R.id.buttonLogin)).perform(click());
+            Thread.sleep(1000); //wait time of 1 sec
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
